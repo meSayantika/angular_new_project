656871743bf5eeac1777d90e1ff5e3ad1859a829
Sayantika Dhar <sayantika.dhar04@gmail.com>
2023-02-08 19:07:48 +0530

    project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,11 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
 export class AppComponent {
   title = 'e-commerce-site';
   show:any ;
+  color: any;
+  selectedItem: any;
+  chooseItem: any;
+  effect:any;
+  selectItem:any;
   customOptions: OwlOptions = {
     loop: true,
     mouseDrag: true,
@@ -33,6 +38,51 @@ export class AppComponent {
     },
     nav: true
   }
+ itemList=[{
+  id:1,
+  img:"https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp",
+  title: "Phone" ,
+  subtitle:"title",
+  description: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+  like: false,
+  incart: 0
+ },
+ {
+  id:2,
+  img:"https://mdbcdn.b-cdn.net/img/new/standard/city/043.webp",
+  title: "Fashion jewellery" ,
+  subtitle:"title",
+  description: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+  like: false,
+  incart: 0
+ },
+ {
+  id:3,
+  img:"https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp",
+  title: "Ethnic sets" ,
+  subtitle:"title",
+  description: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+  like: false,
+  incart: 0
+ },
+ {
+  id:4,
+  img:"https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp",
+  title: "Gadgets" ,
+  subtitle:"title",
+  description: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+  like: false,
+  incart: 0
+ },
+ {
+  id:5,
+  img:"https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp",
+  title: "Tv and application" ,
+  subtitle:"title",
+  description: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+  like: false,
+  incart: 0
+ }]
   mystyle(){
    if(this.show==""){
     this.show=true
@@ -40,6 +90,27 @@ export class AppComponent {
   this.show=false
 }
   }
-  
+  likeAct(id:any){
+    // console.log(id);
+var color =this.itemList.findIndex((item:any)=>item.id==id) 
+this.selectedItem=this.itemList[color]
+this.selectedItem.like = !this.selectedItem.like
+// console.log(this.selectedItem)
+  }
+  cartAct(id:any){
+        // console.log(id);
+        var store= this.itemList.findIndex((item:any)=>item.id==id)
+        // console.log(store)
+        this.chooseItem=this.itemList[store]
+       this.chooseItem.incart= ++this.chooseItem.incart
+      //  console.log(this.chooseItem);
 
+  }
+  eyeAct(id:any){
+    // console.log(id);
+    var effect=this.itemList.findIndex((item:any)=>item.id==id)
+    this.selectItem=this.itemList[effect]
+    console.log(this.selectItem);
+
+  }
 }
